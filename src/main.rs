e9330918c8b8723961c3c2401da4deb6122bbd5e
tre3iii <lilshed3@gmail.com>
2026-01-20 06:56:57 +0000

    add comments for main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() -> types::UnitResult {
     //      error handling done in function def
     io::set_terminal();
 
+    //abstracted trait for getting terminal input
+    //      (dependency injection)
     let mut terminal_event_reader = types::TerminalEventReader::new();
 
     //handle user input for starting timer -> start timer if yes
@@ -18,11 +20,13 @@ fn main() -> types::UnitResult {
             panic!("\n\rError while awaiting startup input: {}.", error);
         });
 
+    //give callback to call based on result
     util::io::handle_startup_choice(result, util::timer::timer).unwrap_or_else(|error| {
         io::clear_terminal();
         panic!("\n\rError handling startup input: {}", error);
     });
 
+    //function that blocks thread from terminating on program end before user presses a key
     util::io::blocking_await_keypress();
     
     //error handling done in function def
