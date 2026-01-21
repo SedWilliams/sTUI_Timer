3d761f8255d3cbaee65c9f7f7e25a02871627b88
Shedrick Williams <lilshed3@gmail.com>
2026-01-20 22:35:49 -0800

    scaffolding for ratatui ui

--- src/util/io/ui/mod.rs
+++ src/util/io/ui/mod.rs
@@ -1,3 +1,9 @@
 /*****************************************
- * UI Definitions, Component architecture
+ * UI/Style Definitions, Component architecture
  *****************************************/
+
+// 'Bits' are reusable UI components
+pub mod bits;
+
+// Styles can be applied to 'Bits'
+pub mod styles;
